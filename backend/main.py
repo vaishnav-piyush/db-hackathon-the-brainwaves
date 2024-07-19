b3637b481a1332e807ec76dab8b65f82e8a3aa8d
Piyush Vaishnav <piyush.vaishnav@breakingwave.io>
2024-07-19 04:41:20 +0100

    added careplan endpoint

--- backend/main.py
+++ backend/main.py
@@ -8,6 +8,7 @@ from openai import chat_with_openai
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
 
+
 @app.route("/")
 def hello_world():
     return jsonify({"message": "Hello, tell me about your dependent with dementia"})
@@ -18,6 +19,7 @@ storage_client = storage.Client()
 bucket_name = 'staging.hack-team-the-brainwaves.appspot.com'  # Replace with your bucket name
 cache = TTLCache(maxsize=100, ttl=60)
 cache['data'] = ''
+
 
 @app.route('/upload', methods=['POST'])
 def upload():
@@ -41,7 +43,7 @@ def upload():
     # print_response(response)
     transcript = return_transcript(response)
 
-    cache['data'] = cache['data'] + '\n' + transcript
+    cache['data'] = cache['data'] + '\\n' + transcript
 
     print(cache['data'])
 
@@ -52,13 +54,15 @@ def upload():
 
     return jsonify({"success": True, "filename": audio.filename, "answer": response}), 200
 
-# @app.route('/careplan', methods=['GET'])
-# def careplan():
-#     question = "Based on this patient data, can you give me a personalised careplan? Can you format the response as a Json with two attributes 'Nutrition' and ''"
-#     cache['data'] = cache['data'] + "\\n" + question
-#     response = chat_with_openai(cache['data'])
-#     print(response)
-#     return jsonify({"success": True, "careplan": response}), 200
+
+@app.route('/careplan', methods=['GET'])
+def careplan():
+    question = "Based on this patient data, can you give me a personalised careplan? Can you format the response as a Json with two attributes 'Nutrition' and ''"
+    cache['data'] = cache['data'] + "\\n" + question
+    response = chat_with_openai(cache['data'])
+    print(response)
+    return jsonify({"success": True, "careplan": response}), 200
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
